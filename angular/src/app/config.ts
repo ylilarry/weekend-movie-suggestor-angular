ca4fd062f768aa53ac6c82947b1f247a2a2a41c0
Yu Li <ylilarry@gmail.com>
2023-04-04 15:57:58 -0400

    Change API to findmovie.chat

--- angular/src/app/config.ts
+++ angular/src/app/config.ts
@@ -1,4 +1,4 @@
 // export const chatServerUrl = "http://localhost:13000/chat";
 // export const movieSearchUrl = "http://localhost:13000/movie";
-export const chatServerUrl = "https://api.weekend-movie.yuli.se/chat";
-export const movieSearchUrl = "https://api.weekend-movie.yuli.se/movie";
+export const chatServerUrl = "https://api.findmovie.chat/chat";
+export const movieSearchUrl = "https://api.findmovie.chat/movie";
